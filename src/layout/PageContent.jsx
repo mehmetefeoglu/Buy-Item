3eee57532fb5335bc93c1e3dcd8adac3361f0b95
mehmetefeoglu <mehmet.efeogludev@gmail.com>
2025-02-12 12:58:16 +0300

    react-router versiyonuna göre fixledim

--- src/layout/PageContent.jsx
+++ src/layout/PageContent.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from '../pages/HomePage.jsx';
+
 
 class PageContent extends React.Component {
   render() {
     return (
       <main className="flex-grow">
-        <Routes>
+        <Switch>
           <Route path="/" element={<HomePage />} />
           {/* Diğer sayfalar için route'lar buraya eklenecek */}
-        </Routes>
+        </Switch>
       </main>
     );
   }
